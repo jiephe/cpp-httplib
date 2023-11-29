8c90a570a4d3e5bb2138adc1ffad6a71d41d8f26
wangguodong <wangguodong@pinming.cn>
2023-11-29 09:41:37 +0800

    json response

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -g main.cpp -lpthread
+g++ -g main.cpp -lpthread -ljsoncpp
--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <sys/prctl.h>
 
+#include <json/json.h>
+
 pid_t getCurrentThreadId()
 {
     return static_cast<pid_t>(::syscall(SYS_gettid));
@@ -17,8 +19,19 @@ int main(void)
     Server svr;
 
     svr.Get("/h1", [](const Request& req, Response& res) {
-        std::cout << "h1 tid: " << getCurrentThreadId() << std::endl;
-        res.set_content("Hello World1! " + std::to_string(getCurrentThreadId()), "text/plain");
+        Json::Value root;
+        root["status"] = "OK";
+        root["code"] = 200;
+        Json::Value data;
+        data["message"] = "Hello, World!";
+        root["data"] = data;
+        root["req_str"] = req.get_header_value("str1");
+        root["req_int"] = Json::Value::UInt64(req.get_header_value_u64("int1"));
+
+        Json::StreamWriterBuilder builder;
+        std::string jsonString = Json::writeString(builder, root);
+        std::cout << "h1 tid: " << getCurrentThreadId() << " jsonstring: " << jsonString << std::endl;
+        res.set_content(jsonString, "text/plain");
     });
 
     svr.Get("/h2", [](const Request& req, Response& res) {
